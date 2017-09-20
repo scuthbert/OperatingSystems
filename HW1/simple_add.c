10935def219a8db6ebfbf80edc8c477067d400dc
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2017-09-20 14:21:34 -0600

    Fixed missing line

--- HW1/simple_add.c
+++ HW1/simple_add.c
@@ -5,8 +5,9 @@
 int n3;
 asmlinkage long sys_simple_add(int n1, int n2, int* result)
 {
+	printk(KERN_ALERT "Values are %d and %d\n", n1, n2)
 	n3 = n1 + n2;
 	copy_to_user(result, &n3, sizeof(int));
-	printk(KERN_ALERT "Result is %d", n3);
+	printk(KERN_ALERT "Result is %d\n", n3);
 	return 0;
 }
